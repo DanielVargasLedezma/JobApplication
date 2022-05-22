bc7feb152cac3d829c12197ce694bca5db191487
valeriatch <71364567+valeriatch@users.noreply.github.com>
2022-05-22 15:51:16 -0600

    botones

--- app/src/main/java/com/example/jobapplication/data/provider/JobFormProvider.kt
+++ app/src/main/java/com/example/jobapplication/data/provider/JobFormProvider.kt
@@ -1,6 +1,7 @@
 package com.example.jobapplication.data.provider
 
 import com.example.jobapplication.data.model.JobForm
+import com.example.jobapplication.data.model.Persona
 
 class JobFormProvider private constructor() {
 
@@ -57,4 +58,9 @@ class JobFormProvider private constructor() {
     fun deleteJobApplication(position: Int) {
         jobForms.removeAt(position)
     }
+
+    fun deleteForm (p: JobForm){
+        jobForms.remove(p)
+    }
+
 }
